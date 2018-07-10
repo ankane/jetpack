eeb26682f92cf5769fd3c1579d518143ad228952
Andrew <acekane1@gmail.com>
2018-07-09 20:35:19 -0700

    Added tests

--- tests/testthat/test-jetpack.R
+++ tests/testthat/test-jetpack.R
@@ -7,13 +7,27 @@ options(repos=list(CRAN="https://cloud.r-project.org/"))
 Sys.setenv(TEST_JETPACK = "true")
 
 test_that("it works", {
-  with_dir(tempdir(), {
-    jetpack::init()
-    jetpack::add("randomForest")
-    jetpack::check()
-    jetpack::install()
-    jetpack::update("randomForest")
-    jetpack::remove("randomForest")
+  tryCatch({
+    with_dir(tempdir(), {
+      jetpack::init()
+      expect(file.exists("DESCRIPTION"))
+      expect(file.exists("packrat.lock"))
+
+      jetpack::add("randomForest")
+      expect(grepl("randomForest", paste(readLines("DESCRIPTION"), collapse="")))
+      expect(grepl("randomForest", paste(readLines("packrat.lock"), collapse="")))
+
+      check <- jetpack::check()
+      expect(check)
+
+      jetpack::install()
+      jetpack::update("randomForest")
+
+      jetpack::remove("randomForest")
+      expect(!grepl("randomForest", paste(readLines("packrat.lock"), collapse="")))
+      expect(!grepl("randomForest", paste(readLines("packrat.lock"), collapse="")))
+    })
+  }, finally={
     packrat::off()
   })
 })
