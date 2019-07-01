085284226bb04e1849ae7dab10027c1aa9667144
Andrew Kane <andrew@chartkick.com>
2019-07-01 10:56:38 -0400

    Fixed test warnings

--- tests/testthat/test-jetpack.R
+++ tests/testthat/test-jetpack.R
@@ -2,40 +2,58 @@ context("jetpack")
 
 library(withr)
 
-app_dir <- file.path(tempdir(), "app")
-packrat_dir <- file.path(tempdir(), "packrat")
-
-dir.create(app_dir)
-dir.create(packrat_dir)
-
-Sys.setenv(TEST_JETPACK="true")
-Sys.setenv(R_PACKRAT_CACHE_DIR=packrat_dir)
+createDir <- function(path) {
+  if (!dir.exists(path)) {
+    dir.create(path)
+  }
+}
 
 contains <- function(file, x) {
   grepl(x, paste(readLines(file), collapse=""))
 }
 
+expectFile <- function(name) {
+  expect(file.exists(name), paste(name, "does not exist"))
+}
+
+expectContains <- function(name, str) {
+  expect(contains(name, str), paste(name, "does not contain", str))
+}
+
+refuteContains <- function(name, str) {
+  expect(!contains(name, str), paste(name, "contains", str))
+}
+
+app_dir <- file.path(tempdir(), "app")
+packrat_dir <- file.path(tempdir(), "packrat")
+
+createDir(app_dir)
+createDir(packrat_dir)
+
+Sys.setenv(TEST_JETPACK="true")
+Sys.setenv(R_PACKRAT_CACHE_DIR=packrat_dir)
+
 test_that("it works", {
   tryCatch({
     with_dir(app_dir, {
       jetpack::init()
-      expect(file.exists("DESCRIPTION"))
-      expect(file.exists("packrat.lock"))
-      expect(file.exists(".Rprofile"))
+      expectFile("DESCRIPTION")
+      expectFile("packrat.lock")
+      expectFile(".Rprofile")
 
       jetpack::add("randomForest")
-      expect(contains("DESCRIPTION", "randomForest"))
-      expect(contains("packrat.lock", "randomForest"))
+      expectContains("DESCRIPTION", "randomForest")
+      expectContains("packrat.lock", "randomForest")
 
       check <- jetpack::check()
-      expect(check)
+      expect(check, "Check should return true")
 
       jetpack::install()
       jetpack::update("randomForest")
 
       jetpack::remove("randomForest")
-      expect(!contains("DESCRIPTION", "randomForest"))
-      expect(!contains("packrat.lock", "randomForest"))
+      refuteContains("DESCRIPTION", "randomForest")
+      refuteContains("packrat.lock", "randomForest")
     })
   }, finally={
     packrat::off()
