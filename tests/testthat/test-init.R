0d97865736a92255795476bef3e10629c645e66a
Andrew Kane <andrew@ankane.org>
2022-01-28 14:05:17 -0800

    Added test for init multiple times [skip ci]

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -10,6 +10,18 @@ test_that("keeps DESCRIPTION", {
   })
 })
 
+test_that("can be called multiple times", {
+  setup({
+    jetpack::init()
+
+    rprofile <- readLines(".RProfile")
+
+    jetpack::init()
+
+    expect_equal(readLines(".RProfile"), rprofile)
+  })
+})
+
 test_that("not initialized", {
   setup({
     expect_error(jetpack::add("DBI"), "This project has not yet been initialized.\nRun 'jetpack::init()' to init.", fixed=TRUE)
