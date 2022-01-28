95a37e54d388415a0219389d1921271329bc566c
Andrew Kane <andrew@ankane.org>
2022-01-28 11:39:26 -0800

    Improved CLI test [skip ci]

--- tests/testthat/test-cli.R
+++ tests/testthat/test-cli.R
@@ -6,23 +6,39 @@ test_that("it works", {
     cli <- cliFile()
     jetpack::cli(file=cli)
 
-    run(cli, "init")
+    output <- run(cli, "init")
+    expectContains(output, "Creating virtual environment")
     expectFile("DESCRIPTION")
     expectFile("renv.lock")
     expectFile(".Rprofile")
 
-    run(cli, "add DBI")
+    output <- run(cli, "add DBI")
+    expectContains(output, "Using DBI")
     expectFileContains("DESCRIPTION", "DBI")
     expectFileContains("renv.lock", "DBI")
 
-    run(cli, "check")
+    output <- run(cli, "check")
+    expectContains(output, "All dependencies are satisfied")
 
-    run(cli, "install")
-    run(cli, "install --deployment")
-    run(cli, "update DBI")
+    output <- run(cli, "install")
+    expectContains(output, "Pack complete")
 
-    run(cli, "remove DBI")
+    output <- run(cli, "install --deployment")
+    expectContains(output, "Pack complete")
+
+    output <- run(cli, "update DBI")
+    expectContains(output, "Updated DBI")
+
+    output <- run(cli, "update")
+    expectContains(output, "All packages are up-to-date")
+
+    output <- run(cli, "remove DBI")
+    expectContains(output, "Removed DBI")
     refuteFileContains("DESCRIPTION", "DBI")
     refuteFileContains("renv.lock", "DBI")
+
+    # TODO test when older version installed
+    output <- run(cli, "update renv")
+    expectContains(output, "Updated renv")
   }, deactivate=FALSE)
 })
