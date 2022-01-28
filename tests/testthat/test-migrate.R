10f72df5cc832339312be1b8cc805d665cfe0564
Andrew Kane <andrew@ankane.org>
2022-01-27 22:50:22 -0800

    Improved migrate test [skip ci]

--- tests/testthat/test-migrate.R
+++ tests/testthat/test-migrate.R
@@ -3,5 +3,17 @@ context("migrate")
 test_that("it works", {
   setup({
     expect_message(jetpack::migrate(), "This project has not yet been packified.")
+
+    write("Package: app", file="DESCRIPTION")
+
+    expect_message(jetpack::migrate(), "packrat.lock does not exist.")
+
+    # write("todo", file="packrat.lock")
+
+    # jetpack::migrate()
+
+    # expectFile("renv.lock")
+
+    # expect_message(jetpack::migrate(), "renv.lock already exists. You should be good to go.")
   })
 })
