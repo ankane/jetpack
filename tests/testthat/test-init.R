7072d0332c4b3a660ca8f356bb0b51803bd9b4ea
Andrew Kane <andrew@ankane.org>
2022-01-28 13:57:40 -0800

    Improved init test [skip ci]

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -1,5 +1,15 @@
 context("init")
 
+test_that("keeps DESCRIPTION", {
+  setup({
+    write("Package: app\nImports:\n    DBI", file="DESCRIPTION")
+
+    jetpack::init()
+
+    expectFileContains("DESCRIPTION", "DBI")
+  })
+})
+
 test_that("not packified", {
   setup({
     expect_error(jetpack::add("DBI"), "This project has not yet been packified.\nRun 'jetpack::init()' to init.", fixed=TRUE)
