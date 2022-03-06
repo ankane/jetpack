535dcbcdbf240938041168d3abb16e15c5fcf812
Andrew Kane <andrew@ankane.org>
2022-03-06 15:40:27 -0500

    Added comment [skip ci]

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,8 @@
 library(testthat)
 library(jetpack)
 
-requireNamespace("renv", quietly = TRUE)
+# for renv reverse dependency check
+# https://github.com/ankane/jetpack/pull/25
+requireNamespace("renv", quietly=TRUE)
 
 test_check("jetpack")
