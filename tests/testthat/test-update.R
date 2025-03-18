eed4fa7192fdacb95fb732d8a53598ffca154d78
Andrew Kane <andrew@ankane.org>
2025-03-17 22:41:25 -0700

    Updated DBI version in tests [skip ci]

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -10,13 +10,13 @@ test_that("it works", {
 
     expect_error(jetpack::update("DBI"), "Cannot find package 'DBI' in DESCRIPTION file")
 
-    jetpack::add("DBI@1.1.2")
-    expectFileContains("DESCRIPTION", "DBI (== 1.1.2)")
+    jetpack::add("DBI@1.2.2")
+    expectFileContains("DESCRIPTION", "DBI (== 1.2.2)")
 
     expect_message(jetpack::outdated(), "DBI")
 
     jetpack::update()
     expectFileContains("DESCRIPTION", "DBI")
-    refuteFileContains("DESCRIPTION", "1.1.2")
+    refuteFileContains("DESCRIPTION", "1.2.2")
   })
 })
