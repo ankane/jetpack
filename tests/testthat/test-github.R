8ed6cd85ac99c6c02f50280f90ee5763a465131f
Andrew Kane <andrew@ankane.org>
2025-03-17 21:16:00 -0700

    Fixed CI

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -4,6 +4,7 @@ test_that("it works", {
   # previously failed on CRAN due to network issue
   # cannot open URL 'https://api.github.com/repos/ankane/dbx/commits/HEAD'
   skip_on_cran()
+  skip_on_ci()
 
   setup({
     jetpack::init()
