28215e0935c25cad605ad4fb0f0b9733770a8cb8
Andrew Kane <andrew@ankane.org>
2025-03-17 21:25:16 -0700

    Fixed CI

--- tests/testthat/test-global.R
+++ tests/testthat/test-global.R
@@ -10,36 +10,36 @@ test_that("it works", {
     cli <- cliFile()
     jetpack::cli(file=cli)
 
-    output <- run(cli, "global add DBI")
+    output <- runCli(cli, "global add DBI")
     expectContains(output, "Installed DBI")
 
-    output <- run(cli, "global list")
+    output <- runCli(cli, "global list")
     expectContains(output, "Using DBI")
 
-    output <- run(cli, "global add DBI@1.1.2")
+    output <- runCli(cli, "global add DBI@1.1.2")
     expectContains(output, "Installed DBI 1.1.2")
 
     # TODO figure out remotes error on CI
     # can't convert package rcmdcheck with RemoteType 'any' to remote
     if (!isWindows()) {
-      output <- run(cli, "global outdated")
+      output <- runCli(cli, "global outdated")
       expectContains(output, "DBI")
     }
 
-    output <- run(cli, "global update DBI")
+    output <- runCli(cli, "global update DBI")
     expectContains(output, "Updated DBI")
 
     # TODO figure out remotes error on CI
     # can't convert package rcmdcheck with RemoteType 'any' to remote
     if (!isWindows()) {
-      output <- run(cli, "global outdated")
+      output <- runCli(cli, "global outdated")
       refuteContains(output, "DBI")
     }
 
-    output <- run(cli, "global remove DBI")
+    output <- runCli(cli, "global remove DBI")
     expectContains(output, "Removed DBI")
 
-    output <- run(cli, "global list")
+    output <- runCli(cli, "global list")
     refuteContains(output, "Using DBI")
   }, deactivate=FALSE)
 })
